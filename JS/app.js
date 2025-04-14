57e32f27b6e67f449f8505c3221b8cfe23c0b691
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-04-14 12:02:10 -0600

    working on problems

---
+++ JS/app.js
@@ -0,0 +1,50 @@
+//  Variable Declarations
+// var is a legacy variable keyword
+var age = 32;
+// "blocked scoped" variable declaration 
+let name = "Kolbie Livingston";
+// constant declaration for variables that should not be updated
+const isStudent = false;
+
+
+// primative data types
+let temperature = 79.6;  // Number
+let greeting = "Hello!"; // String
+let isLoggedIn = false;  // Boolean
+let emptyValue;          // null
+let notDefined;          // undefined 
+
+// basic setup for sending data to HTML console
+console.log("Hey Trey!");
+
+console.log(isLoggedIn);
+
+// arithmetic operators 
+let numOne = 10;
+let numTwo = 3;
+
+let sum = numOne + numTwo;
+console.log(sum); // 13
+let difference = numOne - numTwo;
+console.log(difference); // 7
+let product = numOne * numTwo;
+console.log(product); // 30
+let quotient = numOne / numTwo;
+console.log(quotient); // 3.333333333335
+let remainder = numOne % numTwo;
+console.log(remainder); // 1
+
+// increment and decrement 
+numOne++; 
+console.log(numOne); // 11
+numOne--;
+console.log(numOne); // 10
+
+// assignment operators
+let numThree = 5;
+
+numThree += 2;
+console.log(numThree);
+
+numThree *= 3;
+console.log(numThree);
---
+++ JS/problems.js
@@ -0,0 +1,15 @@
+// problem 1 - Variable Declarations
+
+var age = 32;
+let firstName = "Kolbie";
+const isAStudent = true;
+
+console.log(isAStudent);
+
+//problem 2 - Exploring Data Types
+
+let temp = 80;
+greeting = "Hello";
+isLoggedIn = true;
+
+console.log(isLoggedIn);
