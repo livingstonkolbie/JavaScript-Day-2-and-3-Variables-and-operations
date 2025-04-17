1f0e69fbc8b8bb128a03b2e96102fe15abce5a1e
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-04-17 11:59:36 -0600

    working on day 4 problems

--- JS/app.js
+++ JS/app.js
@@ -48,3 +48,81 @@ console.log(numThree);
 
 numThree *= 3;
 console.log(numThree);
+
+
+
+
+// Day 4: Combining Variables, data  types, and Operators
+
+// Calculating Total Price with Discounts
+
+let originalPrice = 100;
+let discountRate = 0.2;
+let isMember = true;
+
+// console.log(originalPrice);
+
+let discountAmount = originalPrice * discountRate;
+let discountedPrice = originalPrice - discountAmount;
+// console.log(discountedPrice);
+
+// Additional discount for memebers
+
+if (isMember) {
+    discountedPrice -= 10;
+};
+
+console.log("Discounted Price: ", discountedPrice);
+
+// Day 4: problem 1
+
+console.log("Day 4: Problem 1");
+
+let itemPrice = 20;
+let quantity = 1;
+let discount = 0.5;
+
+let subtotal = itemPrice * quantity;
+let totalAmount = subtotal * discount;
+
+console.log("Subtotal: ", subtotal);
+console.log("Discount: ", discount);
+console.log("Total: ", totalAmount);
+
+// Day 4: problem 2
+//gotta comment out this problem to keep moving in the console
+
+console.log("Day 4: Problem 2 - commented out so i can continue in console");
+
+// let celsius = prompt("Enter Temperature in Celsius");
+// celsius = Number(celsius);
+// let fahrenheit = (celsius * 9/5) + 32;
+
+// console.log(celsius,"\u00B0C is", fahrenheit,"\u00B0F");
+// alert(celsius + "°C is " + fahrenheit + "°F");
+
+// Day 4: problem 3
+
+console.log("Day 4: problem 3");
+
+let num = 5;
+let str = "10";
+
+console.log("Sum: ", num + str); // concatenation
+console.log("Difference: ", num - str); // coersion mechanism makes it work
+console.log("Loosely equal?", num == str);
+console.log("Strictly equal?", num === str);
+
+// Day 4: problem 4
+
+console.log("Day 4: problem 4");
+
+let billAmount = 200;
+let tipRate = 0.15;
+let people = 3;
+
+let tipAmount = billAmount * tipRate; 
+let dinnerAmount = billAmount + tipAmount
+
+
+console.log(dinnerAmount);
